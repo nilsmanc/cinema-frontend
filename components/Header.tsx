881a3be8d11e46f1abd104f25bd345f1ddfcf10e
nilsmanc <etereaiberia@gmail.com>
2023-03-29 09:25:40 +0300

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
 import styles from '../styles/Header.module.scss'
 
 const Header = () => {
-  return <div></div>
+  return (
+    <div className={styles.wrapper}>
+      <img className={styles.image} src='https://cdn-icons-png.flaticon.com/512/3418/3418886.png' />
+      <button className={styles.table}>Table</button>
+      <button className={styles.cinemas}>Cinemas</button>
+      <button className={styles.about}>About us</button>
+      <input className={styles.search} placeholder='search' />
+    </div>
+  )
 }
 
 export default Header
