fa3b6a219589d48402d70d5443a2c0bdd4daa76a
nilsmanc <etereaiberia@gmail.com>
2023-04-08 07:53:03 +0300

    about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,39 @@
 import { NextPage } from 'next'
 
+import styles from '../styles/About.module.scss'
+
 const About: NextPage = () => {
-  return <div></div>
+  return (
+    <div className={styles.wrapper}>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio quae consequatur natus
+        excepturi ducimus quas! Commodi dolore odit ut explicabo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, temporibus harum
+        accusantium consequuntur quaerat accusamus, architecto excepturi, deleniti beatae nihil quam
+        qui animi earum natus sequi adipisci modi amet eum!
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, illo?</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est facilis, officia tempora
+        maxime odit libero nesciunt quidem ducimus vel, rerum vero! Tempora est eveniet voluptatibus
+        dolorem reiciendis vitae beatae dolores, debitis libero eligendi similique ullam ad at nam
+        nulla ex commodi hic molestiae exercitationem voluptatum sed ducimus fuga delectus quisquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla totam dolores, voluptates
+        accusantium eligendi cupiditate aliquid aliquam, magnam magni doloremque, repudiandae fugit
+        consequuntur natus dolorem vitae ut laborum odio laboriosam deleniti? Cumque, praesentium
+        minima deleniti laudantium dicta modi est velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quisquam magni doloremque
+        nisi amet alias voluptatem totam laudantium voluptate. Suscipit.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, fuga.</p>
+    </div>
+  )
 }
 
 export default About
